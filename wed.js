533889c1ac539886473c2e7de9e83a3dfa6cfeb5
vanchien0809 <108860155+vanchien-123@users.noreply.github.com>
2023-03-02 22:10:46 +0700

    Add files via upload
    upload source code

---
+++ wed.js
@@ -0,0 +1,11 @@
+
+
+function showMenu() {
+    var navLinks = document.getElementById("navLinks");
+    navLinks.style.right = "0px";
+}
+
+function hideMenu() {
+    var navLinks = document.getElementById("navLinks");
+    navLinks.style.right = "-200px";
+}
